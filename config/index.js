8a20a91533fa14cbba6bd90ed821a5e393966d9e
Mercedes Walsh <sadieswalsh@gmail.com>
2019-11-21 14:54:49 -0800

    initialized jawsdb

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize('mysql://root:root@localhost/burgers_db')
+// module.exports = new Sequelize('mysql://root:root@localhost/burgers_db')
+
+module.exports = new Sequelize('mysql://wh385xpvj3qfj6f4:y8xou0pibt83joov@kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uawhm1nhafn4c3gu')
